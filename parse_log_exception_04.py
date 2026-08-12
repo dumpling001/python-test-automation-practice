dd318c959aef530d9eb667016c215b73ce53f389
dumpling001 <dumpling001@gmail.com>
2026-08-12 20:51:46 +0800

    代码重构parse_log_exception_04.py

--- parse_log_exception_04.py
+++ parse_log_exception_04.py
@@ -1,7 +1,10 @@
-log1 = "TEST=POWER RESULT=FAIL ERROR=Voltage_High"
-log2 = "TEST=POWER RESULT=FAIL"
-log3 = "TEST=POWER ERROR=Voltage_High"
-log4 = "RESULT=FAIL ERROR=Voltage_High"
+# log1 = "TEST=POWER RESULT=FAIL ERROR=Voltage_High"
+# log2 = "TEST=POWER RESULT=FAIL"
+# log3 = "TEST=POWER ERROR=Voltage_High"
+# log4 = "RESULT=FAIL ERROR=Voltage_High"
+
+with open("Error_log.txt", "r", encoding="utf-8") as f:
+    log = f.read()
 
 keywords = ["TEST=", "RESULT=", "ERROR="]
 
@@ -19,10 +22,11 @@ def pick_next_position(keyword_position, keyword):
 def parse_log(log, keywords):
     keyword_position = {}
     results = {}
-    length = len(keywords)
+    #length = len(keywords)
     for keyword in keywords:
-        if log.find(keyword) != -1:
-            keyword_position[keyword] = log.find(keyword)
+        position = log.find(keyword) 
+        if position != -1:
+            keyword_position[keyword] = position
 
 
     for keyword in keywords:
@@ -36,11 +40,13 @@ def parse_log(log, keywords):
             results[keyword.replace("=","")] = value
     return results
 
-results1 = parse_log(log1, keywords)
-results2 = parse_log(log2, keywords)
-results3 = parse_log(log3, keywords)
-results4 = parse_log(log4, keywords)
-print(results1)
-print(results2)
-print(results3)
-print(results4)
+results = parse_log(log, keywords)
+print(results)
+# results1 = parse_log(log1, keywords)
+# results2 = parse_log(log2, keywords)
+# results3 = parse_log(log3, keywords)
+# results4 = parse_log(log4, keywords)
+# print(results1)
+# print(results2)
+# print(results3)
+# print(results4)
