d101c4ab0fbb59e4ca995332eff7ecc726ad5eb1
dumpling001 <dumpling001@gmail.com>
2026-08-12 20:40:05 +0800

    use with open instead of open

--- open_file.py
+++ open_file.py
@@ -1,4 +1,9 @@
-f = open("Error_log.txt", "r", encoding="utf-8")
-content = f.read()
-print(content)
-f.close()
+# f = open("Error_log.txt", "r", encoding="utf-8")
+# content = f.read()
+# print(content)
+# f.close()
+
+with open("Error_log.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
+    print(type(content))
